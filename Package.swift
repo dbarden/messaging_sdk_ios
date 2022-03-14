34d12b1e7c1cd2ce4ef18752dbcb016f3361c054
Daniel Barden <dbarden@seatgeek.com>
2022-03-14 12:19:11 +0100

    Fix dependencies

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "ZendeskMessagingAPISDK",
-                 url: "https://github.com/zendesk/messagingapi_sdk_ios",
-                 .exact("3.8.4")),
+                 url: "https://github.com/dbarden/messagingapi_sdk_ios",
+                 .exact("3.8.5")),
         .package(name: "ZendeskCommonUISDK",
-                 url: "https://github.com/zendesk/commonui_sdk_ios",
+                 url: "https://github.com/dbarden/commonui_sdk_ios",
                  .exact("6.1.3"))
     ],
     targets: [
